92da657fc442cdd0b8b78fed83488bf23951c526
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-18 01:45:50 +0200

    feat: flake secrets management

--- flake.nix
+++ flake.nix
@@ -8,8 +8,22 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in
   {
+    packages.x86_64-linux.default = self.packages.x86_64-linux.cf-secrets;
+    packages.x86_64-linux.cf-secrets = pkgs.stdenv.mkDerivation {
+      name = "secrets";
+      src = self;
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ./secrets.sh $out/bin/secrets
+        chmod +x $out/bin/secrets
+      '';
+    };
+
     devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [ jq ];
+      buildInputs = with pkgs; [
+        self.packages.x86_64-linux.cf-secrets
+        jq
+      ];
     };
   };
 }
