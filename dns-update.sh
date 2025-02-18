984b18ce04b9a5ff622c24d99828c94be7c71294
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-18 17:06:42 +0200

    feat: better dns_update modularization

--- dns-update.sh
+++ dns-update.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-export DNS_RECORD_CONTENT="$(curl ifconfig.me)"
-export DNS_RECORD_NAME="valyntyler.com"
-export DNS_RECORD_ID="$(dnslist | jq -r '.result[] | select(.type == "A") | select(.name == "valyntyler.com") | .id')"
+DNS_RECORD_CONTENT="$(curl ifconfig.me)"
+DNS_RECORD_NAME="valyntyler.com"
+DNS_RECORD_TYPE="A"
+DNS_RECORD_ID="$(dnslist | jq -r '.result[]
+  | select(.type == "'$DNS_RECORD_TYPE'")
+  | select(.name == "'$DNS_RECORD_NAME'")
+  | .id
+')"
 
 curl https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD_ID \
   -X PATCH \
@@ -10,9 +15,9 @@ curl https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD
   -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
   -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
   -d '{
-  "content": "'"$DNS_RECORD_CONTENT"'",
+  "content": "'$DNS_RECORD_CONTENT'",
   "name": "'"$DNS_RECORD_NAME"'",
   "proxied": true,
   "ttl": 3600,
-  "type": "A"
+  "type": "'$DNS_RECORD_TYPE'"
 }' | jq
